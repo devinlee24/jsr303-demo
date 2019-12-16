5378147ce6df556c80384d4d2f5169230cbaa89d
devinlee <devinlee24@foxmail.com>
2019-12-16 17:58:32 +0800

    初始化

---
+++ src/test/java/com/devinlee/jsr303demo/Jsr303DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinlee.jsr303demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsr303DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
